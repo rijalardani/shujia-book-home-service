7c106312e631812e985f69d6bb25da1701ff19d9
rijalazhar <rijalazhar.dwi@gmail.com>
2025-07-22 13:55:40 +0700

    Add models and migrations for Category, HomeService, ServiceBenefit, ServiceTestimonial, BookingTransaction, and TransactionDetails

---
+++ database/migrations/2025_07_22_062609_create_booking_transactions_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('booking_transactions', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('booking_trx_id');
+            $table->string('name');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('started_time');
+
+            $table->date('schedule_at');
+            $table->string('proof');
+            $table->string('post_code');
+            $table->string('city');
+            $table->text('address');
+            $table->unsignedBigInteger('sub_total');
+            $table->unsignedBigInteger('total_amount');
+            $table->unsignedBigInteger('total_tax_amount');
+            $table->boolean('is_paid');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('booking_transactions');
+    }
+};
